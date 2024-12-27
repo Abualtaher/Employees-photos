3b569b864a2f82a0c98dd89305a158f3b58881ad
Abualtaher <wisamaltaher@gmail.com>
2024-12-27 22:32:20 +0100

    starting javaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector("#container");
+
+const imgs = [
+  "imges/jason-leung-HBGYvOKXu8A-unsplash.jpg",
+  "imges/julien-tromeur-6UDansS-rPI-unsplash.jpg",
+  "imges/xu-haiwei-_3KdlCgHAn0-unsplash.jpg",
+];
