65868b6cc0ac05320248e9b4460639d20efa2b75
Abualtaher <wisamaltaher@gmail.com>
2024-12-28 01:11:39 +0100

    function

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ const imgs = [
   "imges/julien-tromeur-6UDansS-rPI-unsplash.jpg",
   "imges/xu-haiwei-_3KdlCgHAn0-unsplash.jpg",
 ];
+
+function gettingImges() {
+  let myImges = "";
+  for (let i = 0; i < imgs.length; i++) {
+    myImges += `<img
+          class="photo"
+          src="${imgs[i]}"
+          alt="Employees photos"
+        />`;
+  }
+  container.innerHTML = myImges;
+}
+
+gettingImges();
